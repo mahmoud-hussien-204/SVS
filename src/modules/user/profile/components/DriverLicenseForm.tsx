0eed4076e12e7a227112023048a6bd779e5bef62
mahmoud-hus <mahmoud.hussein@dafa.sa>
2024-10-01 09:34:03 +0300

    id verification

--- src/modules/user/profile/components/DriverLicenseForm.tsx
+++ src/modules/user/profile/components/DriverLicenseForm.tsx
@@ -1,51 +1,46 @@
-import ImageUploader from './ImageUplaoder'
+import ImageUploader from "./ImageUplaoder";
 
-import ErrorMessage from '@/components/ErrorMessage'
+import ErrorMessage from "@/components/ErrorMessage";
 
-import { FormProvider } from 'react-hook-form';
+import {FormProvider} from "react-hook-form";
 
-import ModalHeader from '@/components/ModalHeader';
+import ModalHeader from "@/components/ModalHeader";
 
-import ModalBody from '@/components/ModalBody';
+import ModalBody from "@/components/ModalBody";
 
-import ModalFooter from '@/components/ModalFooter';
+import ModalFooter from "@/components/ModalFooter";
 
-import useDriverLicenseForm from '../hooks/useDriverLicenseForm';
-import useUserProfile from '../hooks/useUserProfile';
+import useDriverLicenseForm from "../hooks/useDriverLicenseForm";
+import useUserProfile from "../hooks/useUserProfile";
 
 function DriverLicenseForm() {
-  const { handleSubmit, form, isPending } = useDriverLicenseForm();
-  const { data } = useUserProfile();
+  const {handleSubmit, form, isPending} = useDriverLicenseForm();
+  const {data} = useUserProfile();
   return (
     <FormProvider {...form}>
-      <form
-        name='driver-license-form'
-        id='driver-license-form'
-        noValidate
-        onSubmit={handleSubmit}
-      >
+      <form name='driver-license-form' id='driver-license-form' noValidate onSubmit={handleSubmit}>
         <ModalHeader title='Driver License' />
         <ModalBody>
           <div className='mb-2rem grid grid-cols-2 gap-1.25rem'>
             <div>
               <div className='relative'>
-                <ImageUploader name="file_two" title="Front Side" locale={!data?.drive_front.id} />
+                <ImageUploader name='file_two' title='Front Side' locale={!data?.drive_front?.id} />
               </div>
               <ErrorMessage>{form.formState.errors.file_two?.message}</ErrorMessage>
             </div>
             <div>
               <div className='relative'>
-                <ImageUploader name="file_three" title="Back Side" locale={!data?.drive_back.id} />
+                <ImageUploader name='file_three' title='Back Side' locale={!data?.drive_back?.id} />
               </div>
               <ErrorMessage>{form.formState.errors.file_three?.message}</ErrorMessage>
             </div>
           </div>
         </ModalBody>
 
-        {!data?.drive_back.id && <ModalFooter isLoading={isPending} title='Verify' />}
+        {!data?.drive_back?.id && <ModalFooter isLoading={isPending} title='Verify' />}
       </form>
-    </FormProvider >
-  )
+    </FormProvider>
+  );
 }
 
-export default DriverLicenseForm
+export default DriverLicenseForm;
